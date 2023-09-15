285ec2224741f5e6a701c27d55eb511fbef99025
MSv <martin.svanda@speed4trade.com>
2023-09-15 11:46:11 +0200

    Chaining

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Emperor The Mightest";
+
+        name = name.concat(" KunDis");
+
+        System.out.println(name);
+
+    }
+}
